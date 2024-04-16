7bc3dda1f01695190c91f128ca9ad518dafafc48
Damian_G <121656655+agogo345@users.noreply.github.com>
2024-04-16 18:39:03 -0500

    Add files via upload

---
+++ Workshop1/assets/app.js
@@ -0,0 +1,64 @@
+//Mis primeros ejercicios de JavaScript
+
+/*
+*Este es mi comentario en bloque
+*/
+
+// Sección 2: Comentarios, Variables y Tipos de Datos
+
+// Ejercicio 5
+
+var miNumero= 10;
+console.log(miNumero);
+
+// Ejercicio 6
+var miBooleano = true;
+console.log(typeof(miBooleano));
+
+// Ejercicio 7
+var miCadena = "Hola como estas";
+console.log(miCadena.length);
+
+// Ejercicio 8
+var miArray = ["perro",8,["tigre"]];
+console.log(miArray);
+
+// Ejercicio 9
+miObjeto ={
+    nombre:"auto",
+    color:"rojo",
+    numeroMatricula:2234
+};
+
+console.log(miObjeto);
+
+
+
+// Sección 3: Interacción con el Usuario
+// Ejercicio 10
+
+// var nombreUsuario = prompt("ingrese su nombre");
+// alert("hola como estas" + " " + nombreUsuario);
+
+// Ejercicio 11
+// var pregunta = confirm("¿Te gusta Javascript")
+
+// if (pregunta == true){
+//     alert("Me alegro que te guste Javascript")
+// }
+// else{
+//     alert("¿Estas bien?")
+// }
+
+// Sección 4: Uso de console para Depuración
+// Ejercicio 9
+console.warn("Esto solo es una practica")
+
+// Ejercicio 10
+console.error("Error de concatenacion")
+
+// Ejercicio 11
+var numeros =[1,2,3,4,5]
+console.table(numeros)
+
+// Sección 5: Trabajando con el Script en un Archivo Externo
