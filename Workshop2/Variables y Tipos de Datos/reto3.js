8d06869743158eb7e4f12f7e8a46d52e177aa7e5
Damian_G <121656655+agogo345@users.noreply.github.com>
2024-04-18 23:53:31 -0500

    Add files via upload

--- Workshop2/Variables y Tipos de Datos/reto3.js
+++ Workshop2/Variables y Tipos de Datos/reto3.js
@@ -1,36 +1,38 @@
-let budgetInit = parseFloat(prompt("Ingresa, Julian, tu presupuesto inicial"));
+let budgetInit = parseFloat(prompt("Ingresa tu presupuesto inicial"));
 let budgetFood = parseFloat(prompt("Ingresa lo presupuestado para la comida"));
 let budgetPlace = parseFloat(prompt("Ingresa lo presupuestado para el alojamiento"));
 let budgetTrans = parseFloat(prompt("Ingresa lo presupuestado para el transporte"));
 let budgetEmerge = parseFloat(prompt("Ingresa los gastos en caso de emergencia"));
 
 let budgetGastos = budgetFood + budgetPlace + budgetTrans + budgetEmerge;
-let budgetrest = budgetInit - budgetGastos;
+let budgetRest = budgetInit - budgetGastos;
 
-alert(`Tu presupuesto restante es ${budgetrest}`);
+alert(`Tu presupuesto restante es ${budgetRest}`);
 
 let extraProducts = [];
 
-let i = 0;
+let continuarComprando = true;
 
-while (i !== 2) {
-    let move = prompt("¿Quieres seguir comprando? (si/no)");
+while (continuarComprando) {
+    let move = prompt("¿Quieres seguir comprando? (si/no)").toLowerCase();
 
     if (move === "no") {
         alert("Gracias por la compra");
-        i = 2;
+        continuarComprando = false;
     } else if (move === "si") {
-        if (budgetrest > budgetEmerge) {
-            alert("Puedes comprar otro artículo extra");
-            let nameArticle = prompt("¿Cuál es el nombre del producto?");
-            let priceArticle = parseFloat(prompt(`¿Cuál es el precio del ${nameArticle}?`));
-            extraProducts.push({ name: nameArticle, cost: priceArticle });
-            budgetrest -= priceArticle;
+        let nameArticle = prompt("¿Cuál es el nombre del producto?");
+        let priceArticle = parseFloat(prompt(`¿Cuál es el precio de ${nameArticle}?`));
 
-            alert(`Te queda ${budgetrest}`);
+        if (budgetRest >= priceArticle) {
+            extraProducts.push({ name: nameArticle, cost: priceArticle });
+            budgetRest -= priceArticle;
+            alert(`Te queda ${budgetRest}`);
         } else {
-            alert("No tienes suficiente presupuesto para comprar otro artículo extra");
-            i = 2;
+            alert("No tienes suficiente presupuesto para este artículo");
         }
+    } else {
+        alert("Por favor, responde 'si' o 'no'");
     }
+
+   
 }
